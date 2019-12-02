2cd2f1703ad837babaddc3e8a253b70f7d8c5a50
John Major <code@johnmajor.ca>
2019-12-02 01:24:02 -0500

    Refactored so part 1 and 2 can be run and tested independently.

--- src/main/kotlin/ca/johnmajor/aoc2019/Day1.kt
+++ src/main/kotlin/ca/johnmajor/aoc2019/Day1.kt
@@ -3,16 +3,20 @@ package ca.johnmajor.aoc2019
 import java.io.File
 import java.util.stream.IntStream
 
-fun calculateFuel(mass: Int): Int = mass / 3 - 2
+private fun calculateFuel(mass: Int): Int = mass / 3 - 2
 
-fun calculateTotalFuel(mass: Int): Int =
+private fun calculateTotalFuel(mass: Int): Int =
     IntStream.iterate(calculateFuel(mass), { it > 0 }, ::calculateFuel).sum()
 
+fun day1part1(input: IntArray): Int = input.sumBy(::calculateFuel)
+
+fun day1part2(input: IntArray): Int = input.sumBy(::calculateTotalFuel)
+
 fun main() {
-    val sum = File(ClassLoader.getSystemResource("day1-input.txt").file).useLines {
-        it.map(String::toInt).fold(0 to 0) { (p1, p2), mass ->
-            p1 + calculateFuel(mass) to p2 + calculateTotalFuel(mass)
-        }
-    }
-    println("Part 1 Answer: ${sum.first}\nPart 2 Answer: ${sum.second}")
+    val input = File(ClassLoader.getSystemResource("day1-input.txt").file)
+        .readLines()
+        .map(String::toInt)
+        .toIntArray()
+    println("Part 1 Answer: ${day1part1(input)}")
+    println("Part 2 Answer: ${day1part2(input)}")
 }
