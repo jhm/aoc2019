714a1c20f6d6356d381ddcd5ad0be24e124ee087
John Major <code@johnmajor.ca>
2019-12-07 21:15:04 -0500

    Moved IntcodeVM out into a separate file.

--- src/main/kotlin/ca/johnmajor/aoc2019/Day5.kt
+++ src/main/kotlin/ca/johnmajor/aoc2019/Day5.kt
@@ -1,93 +1,6 @@
 package ca.johnmajor.aoc2019
 
 import java.io.File
-import kotlin.math.pow
-
-class IntcodeVM(private val mem: IntArray) {
-
-    enum class Opcode(val opcode: Int) {
-        ADD(1),
-        MUL(2),
-        SAV(3),
-        OUT(4),
-        JIT(5),
-        JIF(6),
-        LT(7),
-        EQ(8),
-        HALT(99);
-
-        companion object {
-            private val lookup = values().associateBy { it.opcode }
-            fun from(opcode: Int): Opcode? = lookup[opcode]
-        }
-    }
-
-    enum class ParameterMode {
-        POSITION,
-        IMMEDIATE;
-
-        companion object {
-            private val lookup = values().associateBy { it.ordinal }
-            fun from(i: Int): ParameterMode? = lookup[i]
-        }
-    }
-
-    private fun set(ip: Int, arg: Int, value: Int) {
-        mem[getIndex(ip, arg)] = value
-    }
-
-    private fun get(ip: Int, arg: Int): Int =
-        mem[getIndex(ip, arg)]
-
-    private fun getIndex(ip: Int, arg: Int): Int =
-        when (getParameterMode(ip, arg)) {
-            ParameterMode.POSITION -> mem[ip + arg]
-            ParameterMode.IMMEDIATE -> ip + arg
-            else -> throw IllegalArgumentException("Invalid parameter mode.")
-        }
-
-    private fun getParameterMode(ip: Int, arg: Int): ParameterMode? =
-        ParameterMode.from((mem[ip] / (10.0).pow(arg + 1) % 10).toInt())
-
-    fun run(input: Iterator<Int>): List<Int> {
-        var ip: Int? = 0
-        val output = mutableListOf<Int>()
-
-        while (ip != null) {
-            ip = when (Opcode.from(mem[ip] % 100)) {
-                Opcode.ADD -> {
-                    set(ip, 3, get(ip, 1) + get(ip, 2))
-                    ip + 4
-                }
-                Opcode.MUL -> {
-                    set(ip, 3, get(ip, 1) * get(ip, 2))
-                    ip + 4
-                }
-                Opcode.SAV -> {
-                    set(ip, 1, input.next())
-                    ip + 2
-                }
-                Opcode.OUT -> {
-                    output.add(get(ip, 1))
-                    ip + 2
-                }
-                Opcode.JIT -> if (get(ip, 1) == 0) ip + 3 else get(ip, 2)
-                Opcode.JIF -> if (get(ip, 1) != 0) ip + 3 else get(ip, 2)
-                Opcode.LT -> {
-                    set(ip, 3, if (get(ip, 1) < get(ip, 2)) 1 else 0)
-                    ip + 4
-                }
-                Opcode.EQ -> {
-                    set(ip, 3, if (get(ip, 1) == get(ip, 2)) 1 else 0)
-                    ip + 4
-                }
-                Opcode.HALT -> null
-                else -> throw IllegalArgumentException("Invalid opcode.")
-            }
-        }
-        return output
-    }
-}
 
 fun main() {
     val input = File(ClassLoader.getSystemResource("day5-input.txt").file)
@@ -96,9 +9,11 @@ fun main() {
         .split(",")
         .map(String::toInt)
 
-    val part1 = IntcodeVM(input.toIntArray()).run(listOf(1).iterator()).lastOrNull()
+    var part1: Int? = null
+    IntcodeVM(input.toIntArray()).run({ 1 }, { part1 = it }) // 9219874
     println("Part 1 Answer: $part1")
 
-    val part2 = IntcodeVM(input.toIntArray()).run(listOf(5).iterator()).lastOrNull()
+    var part2: Int? = null
+    IntcodeVM(input.toIntArray()).run({ 5 }, { part2 = it }) // 5893654
     println("Part 2 Answer: $part2")
 }
