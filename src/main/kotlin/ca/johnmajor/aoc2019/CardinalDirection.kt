77c1526a277c777286afb1bf242f39e90ad44e1f
John Major <code@johnmajor.ca>
2019-12-11 21:01:27 -0500

    Added functions to rotate a cardinal direction.

--- src/main/kotlin/ca/johnmajor/aoc2019/CardinalDirection.kt
+++ src/main/kotlin/ca/johnmajor/aoc2019/CardinalDirection.kt
@@ -3,6 +3,22 @@ package ca.johnmajor.aoc2019
 enum class CardinalDirection {
     NORTH, EAST, SOUTH, WEST;
 
+    fun clockwise(): CardinalDirection =
+        when (this) {
+            NORTH -> EAST
+            EAST -> SOUTH
+            SOUTH -> WEST
+            WEST -> NORTH
+        }
+
+    fun counterClockwise(): CardinalDirection =
+        when (this) {
+            NORTH -> WEST
+            EAST -> NORTH
+            SOUTH -> EAST
+            WEST -> SOUTH
+        }
+
     companion object {
         fun from(s: String): CardinalDirection? =
             if (s.isEmpty()) null else from(s.first())
