1865089b752d2b89befb4e6ef15bb3636bce6ce4
John Major <code@johnmajor.ca>
2019-12-12 21:49:50 -0500

    Added an lcm function.

--- src/main/kotlin/ca/johnmajor/aoc2019/Utils.kt
+++ src/main/kotlin/ca/johnmajor/aoc2019/Utils.kt
@@ -42,4 +42,6 @@ fun <T> List<T>.permutations(): List<List<T>> {
     return result
 }
 
-tailrec fun gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
+tailrec fun gcd(a: Long, b: Long): Long = if (b == 0L) a else gcd(b, a % b)
+
+fun lcm(a: Long, b: Long): Long = a * b / gcd(a, b)
